1eefef7edc1394165161e7c910c2a6cd1910485a
purnesh42h <purnesh.dixit92@gmail.com>
2018-01-27 18:58:16 -0500

    toggle letter visibility

--- memory/assets/js/MemoryGame.jsx
+++ memory/assets/js/MemoryGame.jsx
@@ -2,94 +2,91 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 export default function game_init(root) {
-  ReactDOM.render(<MemoryGame />, root);
+  	ReactDOM.render(<MemoryGame />, root);
 }
 
 class MemoryGame extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { 
-	 tiles: [{
-		letter: 'D', hidden: true, matched: false
-	  }, {
-	  	letter: 'H', hidden: true, matched: false
-	  }, {
-	  	letter: 'B', hidden: true, matched: false
-	  }, {
-	  	letter: 'E', hidden: true, matched: false
-	  }, {
-	  	letter: 'C', hidden: true, matched: false
-	  }, {
-	  	letter: 'C', hidden: true, matched: false
-	  }, {
-	  	letter: 'E', hidden: true, matched: false
-	  }, {
-	  	letter: 'G', hidden: true, matched: false
-	  }, {
-		letter: 'F', hidden: true, matched: false
-	  }, {
-	  	letter: 'G', hidden: true, matched: false
-	  }, {
-	  	letter: 'H', hidden: true, matched: false
-	  }, {
-	  	letter: 'F', hidden: true, matched: false
-	  }, {
-	  	letter: 'D', hidden: true, matched: false
-	  }, {
-	  	letter: 'A', hidden: true, matched: false
-	  }, {
-	  	letter: 'B', hidden: true, matched: false
-	  }, {
-	  	letter: 'A', hidden: true, matched: false
-	  }
-	]
+  	constructor(props) {
+    	super(props);
+    	this.state = { 
+			tiles: [{
+				id: "t1", letter: 'D', hidden: true, matched: false
+			}, {
+	  			id: "t2", letter: 'H', hidden: true, matched: false
+	 	  	}, {
+	  			id: "t3", letter: 'B', hidden: true, matched: false
+	  		}, {
+	  			id: "t4", letter: 'E', hidden: true, matched: false
+	  		}, {
+	  			id: "t5", letter: 'C', hidden: true, matched: false
+	  		}, {
+	  			id: "t6", letter: 'C', hidden: true, matched: false
+	  		}, {
+	  			id: "t7", letter: 'E', hidden: true, matched: false
+	  	  	}, {
+	  			id: "t8", letter: 'G', hidden: true, matched: false
+	  	  	}, {
+				id: "t9", letter: 'F', hidden: true, matched: false
+	  	  	}, {
+	  			id: "t10", letter: 'G', hidden: true, matched: false
+	  	  	}, {
+	  	  		id: "t11", letter: 'H', hidden: true, matched: false
+	  	  	}, {
+	  			id: "t12", letter: 'F', hidden: true, matched: false
+	  	  	}, {
+	  			id: "t13", letter: 'D', hidden: true, matched: false
+	 	   	}, {
+	  			id: "t14", letter: 'A', hidden: true, matched: false
+	  	  	}, {
+	  			id: "t15", letter: 'B', hidden: true, matched: false
+	  	  	}, {
+	  			id: "t16", letter: 'A', hidden: true, matched: false
+	  	  	}
+		]
     }
   }
   
+  toggleVisibility(id) {
+	  let xs = _.map(this.state.tiles, (tile) => {
+	  	if (tile.id == id) {
+	    	return _.extend(tile, {hidden: false});
+	    } else {
+			return tile;
+	    }
+	 });
+	 this.setState({ tiles: xs });
+  }
+    
   render() {
-	  return (
-                <div>
-	        <div className="row game-row">
-	            <div className="col-3 game-col">
-		    </div>
-	            <div className="col-3 game-col">
-	            </div>
-	            <div className="col-3 game-col">
-	            </div>
-	            <div className="col-3 game-col">
-	            </div>
-	          </div>
-                  <div className="row game-row">
-		    <div className="col-3 game-col">
-                    </div>
-                    <div className="col-3 game-col">
-                    </div>
-                    <div className="col-3 game-col">
-                    </div>
-                    <div className="col-3 game-col">
-                    </div>
-		  </div>
-                  <div className="row game-row">
-                    <div className="col-3 game-col">
-                    </div>
-                    <div className="col-3 game-col">
-                    </div>
-                    <div className="col-3 game-col">
-                    </div>
-                    <div className="col-3 game-col">
-                    </div>
-                  </div>
-                  <div className="row game-row">
-                    <div className="col-3 game-col">
-                    </div>
-                    <div className="col-3 game-col">
-                    </div>
-                    <div className="col-3 game-col">
-                    </div>
-                    <div className="col-3 game-col">
-                    </div>
-                  </div>
-		</div>
+    let tile_list = _.map(this.state.tiles, (id, letter, hidden, matched) => {
+	        return <Tile id={id} click={this.toggleVisibility.bind(this)} id={id} hidden={hidden} matched={matched} />;
+	});
+    return (
+	  <div className="row game-row">
+	    { tile_list }  
+	  </div>
+    )
+  }
+}
+
+function Tile(params) {
+  if (params.matched) {
+    return (
+	  <div className="col-3 game-col-matched">
+	    <p id = {params.id}></p>
+      </div>
+	)
+  } else if (params.hidden) {
+    return (
+	  <div className="col-3 game-col" onclick={() => params.click(params.id)}>
+	    <p id = {params.id}></p>
+      </div>
+    ) 
+  } else {
+      return (
+  	  <div className="col-3 game-col">
+  	    <p id = {params.id}>{params.letter}</p>
+      </div>
     )
   }
 }
