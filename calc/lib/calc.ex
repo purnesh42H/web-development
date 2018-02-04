f1dea2f540f4de6f5ff4a2f4c9ae2f5008a9c1e6
purnesh42h <purnesh.dixit92@gmail.com>
2018-02-04 01:20:11 -0500

    Stack creation first draft

--- calc/lib/calc.ex
+++ calc/lib/calc.ex
@@ -1,4 +1,7 @@
 defmodule Calc do
+  defstruct op_stack: []
+  defstruct num_stack: []
+
   @moduledoc """
   Documentation for Calc.
   """
@@ -12,8 +15,37 @@ defmodule Calc do
       :world
 
   """
+
+  def parse(line) do
+    line
+    |> String.split()
+    |> String.to_charlist
+  end
+
   def eval(line) do
-    line  
+    line
+    |> parse
+    |> create_stacks()
+  end
+
+  def create_stacks(char_list) do
+    n = length(char_list)
+    char_list
+    |> stack_push(n, [], [])
+  end
+
+  def stack_push(char_list, n, op_stack, num_stack) when n == 0
+    {op_stack, num_stack}
+  end
+
+  def stack_push(char_list, n, op_stack, num_stack) when char == '*' or char == '-' or char == '+' or char == '/' do
+    op_stack ++ [hd char_list]
+    stack_push(tl char_list, n - 1, op_stack, num_stack)
+  end
+
+  def stack_push(char_list, n, op_stack, num_stack) do
+    num_stack ++ [hd char_list]
+    stack_push(tl char_list, n - 1, op_stack, num_stack)
   end
 
   def main() do
@@ -23,7 +55,7 @@ defmodule Calc do
       {:error, reason} ->
         IO.puts "Error: #{reason}"
       line ->
-        IO.puts eval(line)
+        IO.inspect eval(line)
         main()
     end
   end
