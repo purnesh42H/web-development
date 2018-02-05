2a53d4e1ed33371465daf632f49a87113204382d
purnesh42h <purnesh.dixit92@gmail.com>
2018-02-05 00:58:54 +0000

    Simple addition working

--- calc/lib/calc.ex
+++ calc/lib/calc.ex
@@ -31,50 +31,51 @@ defmodule Calc do
   end
 
   def evaluate(op1, op2, "+") do
-    Integer.parse(op1) + Integer.parse(op2)
+    op1 + op2
   end
 
   def evaluate(op1, op2, "-") do
-    Integer.parse(op1) + Integer.parse(op2)
+    op1 - op2
   end
 
   def evaluate(op1, op2, "*") do
-    Integer.parse(op1) + Integer.parse(op2)
+    op1 * op2
   end
 
   def evaluate(op1, op2, "/") do
-    Integer.parse(op1) + Integer.parse(op2)
+    op1 / op2
   end
 
   def operate(op1, op2, operator) do
     evaluate(op1, op2, operator)
   end
 
-  def calculate(expression_list, exp_len, num_stack, num_len, op_stack, op_len, head_char) do when exp_len ==  0
-      op2 = head_char
+  def calculate(expression_list, exp_len, num_stack, num_len, op_stack, op_len, head_char) when exp_len ==  0 do
+      {op2, decimal} = Integer.parse(head_char)
       op1 = hd num_stack
       operate(op1, op2, (hd op_stack))
   end
 
-  def calculate(expression_list, exp_len, num_stack, num_len, op_stack, op_len, head_char) do when head_char == "+" and op_len == 0
+  def calculate(expression_list, exp_len, num_stack, num_len, op_stack, op_len, head_char) when head_char == "+" and op_len == 0 do
       calculate((tl expression_list), exp_len - 1,
       num_stack, num_len, op_stack ++ [head_char], op_len + 1, (hd expression_list))
   end
 
-  def calculate(expression_list, exp_len, num_stack, num_len, op_stack, op_len, head_char) do when head_char == "+"
+  def calculate(expression_list, exp_len, num_stack, num_len, op_stack, op_len, head_char) when head_char == "+" do
       op2 = (hd num_stack)
       op1 = (hd (tl num_stack))
-      res = operator(op1, op2, (hd op_stack))
+      res = operate(op1, op2, (hd op_stack))
       num_stack = (tl (tl num_stack)) ++ [res]
-      op_stack = (tl op_stack) + [head_char]
+      op_stack = (tl op_stack) ++ [head_char]
       calculate((tl expression_list), exp_len - 1,
         num_stack, num_len - 2, op_stack, op_len, (hd expression_list))
   end
 
 
-  def calculate(expression_list, exp_len, num_stack, num_len, op_stack, op_len, head_char) do when num_len == 0
+  def calculate(expression_list, exp_len, num_stack, num_len, op_stack, op_len, head_char)  do 
+    {int, decimal} = Integer.parse(head_char)
     calculate((tl expression_list), exp_len - 1,
-      num_stack ++ [head_char], num_len + 1, op_stack, op_len, (hd expression_list))
+      num_stack ++ [int], num_len + 1, op_stack, op_len, (hd expression_list))
   end
 
   def calculator(expression_list) do
