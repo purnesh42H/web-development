4e4f81170097f3f92067a425f6d61eedaaf40841
purnesh42h <purnesh.dixit92@gmail.com>
2018-02-11 00:42:14 -0500

    Hopefully working

--- memory/lib/memory/game.ex
+++ memory/lib/memory/game.ex
@@ -1,41 +1,42 @@
 defmodule Memory.Game do
   def new do
-  %{
-    tiles: [%{
-      id: "t1", letter: "D", hidden: true, matched: false
-    }, %{
-      id: "t2", letter: "H", hidden: true, matched: false
-    }, %{
-      id: "t3", letter: "B", hidden: true, matched: false
-    }, %{
-      id: "t4", letter: "E", hidden: true, matched: false
-    }, %{
-      id: "t5", letter: "C", hidden: true, matched: false
-    }, %{
-      id: "t6", letter: "C", hidden: true, matched: false
-    }, %{
-      id: "t7", letter: "E", hidden: true, matched: false
-    }, %{
-      id: "t8", letter: "G", hidden: true, matched: false
-    }, %{
-      id: "t9", letter: "F", hidden: true, matched: false
-    }, %{
-      id: "t10", letter: "G", hidden: true, matched: false
-    }, %{
-      id: "t11", letter: "H", hidden: true, matched: false
-    }, %{
-      id: "t12", letter: "F", hidden: true, matched: false
-    }, %{
-      id: "t13", letter: "D", hidden: true, matched: false
-    }, %{
-      id: "t14", letter: "A", hidden: true, matched: false
-    }, %{
-      id: "t15", letter: "B", hidden: true, matched: false
-    }, %{
-      id: "t16", letter: "A", hidden: true, matched: false
-    }],
-    clicks: 0,
-  }
+    %{
+      tiles: [%{
+        id: "t1", letter: "D", hidden: true, matched: false
+      }, %{
+        id: "t2", letter: "H", hidden: true, matched: false
+      }, %{
+        id: "t3", letter: "B", hidden: true, matched: false
+      }, %{
+        id: "t4", letter: "E", hidden: true, matched: false
+      }, %{
+        id: "t5", letter: "C", hidden: true, matched: false
+      }, %{
+        id: "t6", letter: "C", hidden: true, matched: false
+      }, %{
+        id: "t7", letter: "E", hidden: true, matched: false
+      }, %{
+        id: "t8", letter: "G", hidden: true, matched: false
+      }, %{
+        id: "t9", letter: "F", hidden: true, matched: false
+      }, %{
+        id: "t10", letter: "G", hidden: true, matched: false
+      }, %{
+        id: "t11", letter: "H", hidden: true, matched: false
+      }, %{
+        id: "t12", letter: "F", hidden: true, matched: false
+      }, %{
+        id: "t13", letter: "D", hidden: true, matched: false
+      }, %{
+        id: "t14", letter: "A", hidden: true, matched: false
+      }, %{
+        id: "t15", letter: "B", hidden: true, matched: false
+      }, %{
+        id: "t16", letter: "A", hidden: true, matched: false
+      }],
+      clicks: 0,
+    }
+  end
 
   def get_guess_tile(tiles, id) do
     Enum.find tiles, fn(tile) ->
@@ -68,6 +69,7 @@ defmodule Memory.Game do
       else
         tile
       end
+    end
   end
 
   def tiles_after_match(tiles, guess, active, is_correct_match?) do
@@ -79,6 +81,7 @@ defmodule Memory.Game do
       else
         tile
       end
+    end
   end
 
   def matchTile(tiles, guess_id) do
@@ -103,13 +106,13 @@ defmodule Memory.Game do
     |> toggle_visibility(id)
     |> matchTile(id)
     Map.put(game, :tiles, tiles)
-    |> Map.put(:clicks, clicks + 1)
+    |> Map.put(:clicks, Map.fetch!(game, :clicks, clicks + 1))
   end
 
   def flip(game, id) do
     tiles = game.tiles
     |> toggle_visibility(id)
     Map.put(game, :tiles, tiles)
-    |> Map.put(:clicks, clicks + 1)
+    |> Map.put(:clicks, Map.fetch!(game, :clicks, clicks + 1))
   end
 end
